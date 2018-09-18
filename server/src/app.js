f07138336980a34de9c7ea82a03a3512cf4d70e4
William Huang <shsidforever@gmail.com>
2018-09-18 13:42:57 +0800

    set up db

--- server/src/app.js
+++ server/src/app.js
@@ -2,15 +2,17 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+const { sequelize } = require('./models')
+const config = require('./config/config')
 
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.post('/register', (req, res) => {
-  res.send({
-    message: `Hello ${req.body.email}! your user was registered! Have fun`
+require('./routes')(app)
+sequelize.sync()
+  .then(() => {
+    app.listen(config.port)
+    console.log(`port started at ${config.port}`)
   })
-})
-app.listen(process.env.PORT || 8082)
